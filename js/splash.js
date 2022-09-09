988dea073caa552e3516893a8105ad05aa45a84d
MasakiOkumura <masakiokumura26@gmail.com>
2022-09-09 18:49:56 +0900

    iriirp

--- js/splash.js
+++ js/splash.js
@@ -4,19 +4,21 @@ setTimeout(function(){
 
 $(function(){
     const loadingBox = document.getElementById('loadingbox');
+    const loading = document.getElementById('loading');
 
     // タイピングアニメーション
     $('#splashtext').t({
         speed_vary: true,
         // 終了時ローディングボックスを表示&ローディングアニメーション開始
         fin: function(){
+            loading.style.visibility = 'visible';
             loadingBox.style.visibility = 'visible';
             $('#loadingline').addClass('loadingline2');
         }
     });
     // ローディングアニメーション終了時2.1秒待って要素を削除する
     loadingBox.addEventListener('animationend', () => {
-        setTimeout(function(){loadingBox.style.display = 'none';}, 2100);
+        setTimeout(function(){loadingBox.style.display = 'none';}, 3100);
         
     });
 });
